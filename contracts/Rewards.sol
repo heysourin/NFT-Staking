552cefe1efc263ced5166cb1f356ebbdf93c893b
heysourin <sourin07@gmail.com>
2023-04-02 23:22:27 +0530

    ~

--- contracts/Rewards.sol
+++ contracts/Rewards.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-
 contract Rewards is ERC20, ERC20Burnable, Ownable {
     mapping(address => bool) controllers;
 
